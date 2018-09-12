4bb8ddb5e13f9dd950d251a20237c9ab912bd15d
Sarah Snyder <sarah.s714@stu.nebo.edu>
2018-09-12 10:27:15 -0600

    fixed the jump mechanic and added the A & D movement mechanic

--- 2D Project/Assets/Scripts/PlayerControl.cs
+++ 2D Project/Assets/Scripts/PlayerControl.cs
@@ -2,15 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControl : MonoBehaviour {
+public class PlayerControl : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Player Movement Variables
+    public int MoveSpeed;
+    public float JumpHeight;
+
+    //Player grounded variables
+    public Transform groundcheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundcheck.position, groundCheckRadius, whatIsGround);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // This code makes the character jump
+        if (Input.GetKeyDown (KeyCode.Space)&& grounded){
+            Jump();
+        }
+
+        //this code makes the character move from side to side using the A&D keys
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        }
+
+
+    }
+
+
+
+    public void Jump(){
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
+    }
 }
