5585b85bc4ba1d3221f3f77a9f45d8d211555eb0
Sarah Snyder <sarah.s714@stu.nebo.edu>
2018-09-18 10:28:02 -0600

    added the double jump and fixed errors

--- 2D Project/Assets/Scripts/PlayerControl.cs
+++ 2D Project/Assets/Scripts/PlayerControl.cs
@@ -8,12 +8,17 @@ public class PlayerControl : MonoBehaviour
     // Player Movement Variables
     public int MoveSpeed;
     public float JumpHeight;
+    private bool doubleJump;
 
     //Player grounded variables
     public Transform groundcheck;
     public float groundCheckRadius;
     public LayerMask whatIsGround;
     private bool grounded;
+
+    //Non-Stick Player
+    private float moveVelocity;
+
 
     // Use this for initialization
     void Start()
@@ -33,28 +38,42 @@ public class PlayerControl : MonoBehaviour
     {
 
         // This code makes the character jump
-        if (Input.GetKeyDown (KeyCode.Space)&& grounded){
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
             Jump();
         }
 
+        // Double Jump code
+        if(grounded)
+            doubleJump = false;
+
+        if(Input.GetKeyDown (KeyCode.Space)&& !doubleJump && !grounded){
+            Jump();
+            doubleJump = true;
+        }
+
+        //Non-Stick Player
+        moveVelocity = 0f;
+
         //this code makes the character move from side to side using the A&D keys
-        if (Input.GetKey(KeyCode.D))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-
+        if (Input.GetKey (KeyCode.D)){
+        //GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        moveVelocity = MoveSpeed;
         }
-        if (Input.GetKey(KeyCode.A))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 
+        if (Input.GetKey (KeyCode.A)){
+            //GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            moveVelocity = -MoveSpeed;
         }
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
 
 
     }
 
-
-
-    public void Jump(){
+    public void Jump()
+    {
         GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
     }
-}
+
+    }
